656d41134b01651d08b4490de7ce86ef0c7b6d92
PlatPlat48 <roberts.chance@gmail.com>
2017-05-19 13:42:26 -0600

    DataMap Attempt?
    MongoDB database has been added to, but it probably wasn’t actually
    done correctly… :|

--- src/main/scala/DataMap.scala
+++ src/main/scala/DataMap.scala
@@ -7,39 +7,41 @@ import com.mongodb.casbah.Imports._
 abstract class DataMap[K,V](val databaseName: String, val tableName: String, val username: String, val password: String, val IP: String, val port: Int) {
   def get(k: K) : Option[V]
 
-  def getM(k: K) : V
+  def getM(k: K) : V = get(k) match{
+    case Some(x) => x
+    case None => throw new Exception
+  }
 
   def put(k: K, v: V) : Unit
 }
 
-class MongoDB[K,V: MongoDBObject](val dName: String, val tName: String, val uName: String = "", val pssWord: String = "", val ip: String = "localhost", val prt: Int = 27017) extends DataMap[K,V](dName, tName, uName, pssWord, ip, prt) {
+class MongoDB[K](val dName: String, val tName: String, val uName: String = "", val pssWord: String = "", val ip: String = "localhost", val prt: Int = 27017) extends DataMap[K,DBObject](dName, tName, uName, pssWord, ip, prt) {
   val client = MongoClient(MongoClientURI{
     val startingPoint = "mongodb://"
     val addUserPass = (username, password) match{
       case ("", "") => startingPoint
       case _ => startingPoint + username + ":" + password + "@"
     }
-    addUserPass + ip + ":" + port.toString() + "/"
+    addUserPass + ip + ":" + port.toString + "/"
   })
-  val database = client(databaseName)
-  val coll = database(tableName)
+  private val database = client(databaseName)
+  private val coll = database(tableName)
 
-  def get(k: K) : Option[MongoDBObject] = ???
+  def get(k: K) : Option[DBObject] = {
+    coll.findOne(MongoDBObject("_id" -> k))
+  }
 
-  def getM(k: K) : V = ???
-
-  def put(k: K, v: V) : Unit = ???
+  def put(k: K, v: DBObject) : Unit = {
+    v.put("_id", k)
+    coll.insert(v)
+    ()
+  }
 }
 
 //class Heap[K,V](val tName: String = "", val uName: String = "", val pssWord: String = "", val ip: String = "", val prt: Int = 0) extends DataMap[K,V](tName, uName, pssWord, ip, prt) {
 class Heap[K,V] extends DataMap[K,V]("","","","","",0){
   var map: Map[K,V] = Map.empty
   def get(k: K) : Option[V] = map.get(k)
-
-  def getM(k: K) : V = map.get(k) match{
-    case None => throw new Exception
-    case Some(x) => x
-  }
 
   def put(k: K, v: V) : Unit = {
     map = map + (k->v)
@@ -50,8 +52,6 @@ class Heap[K,V] extends DataMap[K,V]("","","","","",0){
 //class Null[K,V](val tName: String = "", val uName: String = "", val pssWord: String = "", val ip: String = "", val prt: Int = 0) extends DataMap[K,V](tName, uName, pssWord, ip, prt) {
 class Null[K,V] extends DataMap[K,V]("","","","","",0){
   def get(k: K) : Option[V] = Some(k.asInstanceOf[V])
-
-  def getM(k: K) : V = k.asInstanceOf[V]
 
   def put(k: K, v: V) : Unit = ()
 }
