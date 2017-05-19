cf71007197a6acda932cadb0fae4158603ae65e7
PlatPlat48 <roberts.chance@gmail.com>
2017-05-19 11:08:28 -0600

    Added MongoDB Stuff; Started to Implement MongoDB Class
    Self-explanatory

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.12.2"
 
-//libraryDependencies +=
-//  "com.typesafe.akka" % "akka-actor" % "2.5.1"
+libraryDependencies += "org.mongodb" %% "casbah" % "3.1.1"
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.5.1"
+//libraryDependencies += "org.mongodb" %% "mongo-scala-driver" % "2.0.0"
+
         
--- src/main/scala/DataMap.scala
+++ src/main/scala/DataMap.scala
@@ -1,8 +1,10 @@
 /**
   * Created by chanceroberts on 5/18/17.
   */
-abstract class DataMap[K,V](val tableName: String, val IP: String, val port: Int) {
 
+import com.mongodb.casbah.Imports._
+
+abstract class DataMap[K,V](val databaseName: String, val tableName: String, val username: String, val password: String, val IP: String, val port: Int) {
   def get(k: K) : Option[V]
 
   def getM(k: K) : V
@@ -10,32 +12,43 @@ abstract class DataMap[K,V](val tableName: String, val IP: String, val port: Int
   def put(k: K, v: V) : Unit
 }
 
-class MongoDB[K,V](val tName: String, val ip: String, val prt: Int) extends DataMap[K,V](tName, ip, prt) {
-  def get(k: K) : Option[V] = ???
+class MongoDB[K,V: MongoDBObject](val dName: String, val tName: String, val uName: String = "", val pssWord: String = "", val ip: String = "localhost", val prt: Int = 27017) extends DataMap[K,V](dName, tName, uName, pssWord, ip, prt) {
+  val client = MongoClient(MongoClientURI{
+    val startingPoint = "mongodb://"
+    val addUserPass = (username, password) match{
+      case ("", "") => startingPoint
+      case _ => startingPoint + username + ":" + password + "@"
+    }
+    addUserPass + ip + ":" + port.toString() + "/"
+  })
+  val database = client(databaseName)
+  val coll = database(tableName)
 
-  def getM(k: K) : Option[V] = ???
+  def get(k: K) : Option[MongoDBObject] = ???
+
+  def getM(k: K) : V = ???
 
   def put(k: K, v: V) : Unit = ???
 }
 
-class Heap[K,V](val tName: String, val ip: String = "", val prt: Int = 0) extends DataMap[K,V](tName, ip, prt) {
-  //Probably needs to be restructured so we don't use var.
+//class Heap[K,V](val tName: String = "", val uName: String = "", val pssWord: String = "", val ip: String = "", val prt: Int = 0) extends DataMap[K,V](tName, uName, pssWord, ip, prt) {
+class Heap[K,V] extends DataMap[K,V]("","","","","",0){
   var map: Map[K,V] = Map.empty
   def get(k: K) : Option[V] = map.get(k)
 
   def getM(k: K) : V = map.get(k) match{
-    case None => throw Exception
+    case None => throw new Exception
     case Some(x) => x
   }
 
   def put(k: K, v: V) : Unit = {
-    //Needs to be redone, probably, to keep this as functional programming.
     map = map + (k->v)
     ()
   }
 }
 
-class Null[K,V](val tName: String, val ip: String, val prt: Int) extends DataMap[K,V](tName, ip, prt) {
+//class Null[K,V](val tName: String = "", val uName: String = "", val pssWord: String = "", val ip: String = "", val prt: Int = 0) extends DataMap[K,V](tName, uName, pssWord, ip, prt) {
+class Null[K,V] extends DataMap[K,V]("","","","","",0){
   def get(k: K) : Option[V] = Some(k.asInstanceOf[V])
 
   def getM(k: K) : V = k.asInstanceOf[V]
