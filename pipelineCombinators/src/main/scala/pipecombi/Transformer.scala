6199e10311d3217f960193c88add5f8ee439a6bb
PlatPlat48 <roberts.chance@gmail.com>
2017-07-03 15:23:16 -0600

    Added Error Tracking, Validated Default Multi-Threading
    Added capabilities for an error tracker, with a list of exceptions
    which will be compared with the Errors. (If there are actually errors
    that are in there. If not, then the default code will still occur)
    Finished my multi-threading Akka code and verified that the default
    configurations of that code worked. I still have yet to try adding a
    config file and have yet to test Error Tracking, though…

--- pipelineCombinators/src/main/scala/pipecombi/Transformer.scala
+++ pipelineCombinators/src/main/scala/pipecombi/Transformer.scala
@@ -2,10 +2,11 @@ package pipecombi
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.util.Timeout
 import akka.pattern.ask
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigObject}
 
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
+import scala.collection.JavaConverters
 import pipecombi._
 
 import scala.concurrent.{Await, Future}
@@ -40,29 +41,40 @@ case class Transformation[Input <: Identifiable,Output <: Identifiable](proc: Tr
 
 
 abstract class IncrTransformer[Input <: Identifiable , Output <: Identifiable](c: Option[Config] = None) extends Transformer[Input, Output] {
-  val actorSys: ActorSystem = ActorSystem.apply(ConfigHelper.possiblyInConfig(c, "ActorSystemName", ""), c)
+  val actorSys: ActorSystem = ActorSystem.apply(ConfigHelper.possiblyInConfig(c, "ActorSystemName", "Increment"), c)
   implicit val executionContext = actorSys.dispatcher
+  implicit val timeout = Timeout(10 hours)
+  val errList: List[ErrorSummary] = List.empty[ErrorSummary]
+  def addAnActor(numberOfCores: Integer, list: List[ActorRef]): List[ActorRef] = numberOfCores match {
+    case x if x == 0 => list
+    case x => addAnActor(numberOfCores-1, actorSys.actorOf(Props(new FunctionActor(compute)), "FunctionActor"+(numberOfCores-1).toString) :: list)
+  }
   val actorList: List[ActorRef] = c match{
     case None =>  //default
       //I know I have 4 cores on this machine, so for now, I'm going to say that it's 4.
       //Until I figure out a way to get the number of cores on a machine, this will be the default behavior, I guess...
-      def addAnActor(numberOfCores: Integer, list: List[ActorRef]): List[ActorRef] = {
-        addAnActor(numberOfCores-1, actorSys.actorOf(Props(new FunctionActor(compute)), "FunctionActor"+(numberOfCores-1).toString) :: list)
-      }
       addAnActor(4, List.empty[ActorRef])
     case Some(conf) =>
       try {
         val newConf = conf.getObject("akka").toConfig.getObject("actor").toConfig.getObject("deployment").toConfig
+        /*val actorMap = JavaConverters.mapAsScalaMap(actorsToImplement.unwrapped())
+        actorMap.foldRight(List.empty[ActorRef]){
+          case ((s, aRef), list) => aRef match{
+              case c: ConfigObject => c.
+            }
+          }
+        }*/
         val numberOfActors = ConfigHelper.possiblyInConfig(c, "numberOfRemoteActors", 0)
-        def buildAnActorList(aList: List[ActorRef], actorsLeft: Int): List[ActorRef] = actorsLeft match {
+        def buildAnActorList(aList: List[ActorRef], actorsLeft: Int, prefix: String): List[ActorRef] = actorsLeft match {
           case 0 => aList
-          case x => actorSys.actorOf(Props(new FunctionActor(compute)), ConfigHelper.possiblyInConfig(c, "remoteActor"+actorsLeft, "remoteActor"+actorsLeft)) :: aList
+          case x => actorSys.actorOf(Props(new FunctionActor(compute)), ConfigHelper.possiblyInConfig(c, prefix+actorsLeft, prefix+actorsLeft)) :: aList
         }
-        val remoteList = buildAnActorList(List.empty[ActorRef], numberOfActors)
+        val remoteList = buildAnActorList(List.empty[ActorRef], numberOfActors, "remoteActor")
         //Next step: Local Actors! :)
-        ???
+        val numberOfLocalActors = ConfigHelper.possiblyInConfig(c, "numberOfLocalActors", 0)
+        buildAnActorList(remoteList, numberOfLocalActors, "localActor")
       } catch{
-        case e: Exception => ???
+        case e: Exception => addAnActor(4, List.empty[ActorRef])
       }
       //Get all of the actors out of the Actor System somehow.
   }
@@ -81,6 +93,7 @@ abstract class IncrTransformer[Input <: Identifiable , Output <: Identifiable](c
       case ex:Exception => {
         // Compute exception occurred, log this in error store
         errMap.put(input.identity, GeneralErrorSummary(ex))
+        statMap.put(input.identity, Error)
         List()
       }
     }
@@ -89,8 +102,8 @@ abstract class IncrTransformer[Input <: Identifiable , Output <: Identifiable](c
   def multiComputeThenStore(inputs: List[Input], outputMap: DataMap[Output]): List[Output] = {
     //The way I wrote this, each future's index corresponds to inputs.reverse's index.
     def multiComputeThenStoreHelper(inputs: List[Input], actors: List[ActorRef], maxCores: Integer, futures: List[Future[Any]]): (List[Input], List[Future[Any]]) = (inputs.length, actors.length) match {
-      case (0, _) => (inputs, List.empty[Future[List[Output]]])
-      case (_, 0) => (inputs, List.empty[Future[List[Output]]])
+      case (0, _) => (inputs, futures)
+      case (_, 0) => (inputs, futures)
       case (_, n) =>
         val fut = actors.head ? inputs.head
         val currList = fut :: futures
@@ -98,7 +111,7 @@ abstract class IncrTransformer[Input <: Identifiable , Output <: Identifiable](c
         (inputsLeft, n) match{
           case (lis, _) if lis.isEmpty => (lis, fs)
           case (_, x) if x == maxCores => multiComputeThenStoreHelper(inputsLeft, actors, maxCores, fs)
-          case (_, x) => (inputs.tail, fs)
+          case (_, x) => (inputsLeft, fs)
         }
     }
     val (_, futures) = multiComputeThenStoreHelper(inputs, actorList, actorList.length, List.empty[Future[Any]])
@@ -110,6 +123,7 @@ abstract class IncrTransformer[Input <: Identifiable , Output <: Identifiable](c
         future.value match {
           case Some(Success(e: Exception)) =>
             errMap.put(input.identity, GeneralErrorSummary(e))
+            statMap.put(input.identity, Error)
             oList
           case Some(Success(l: List[Output])) =>
             l.foldRight(oList){
@@ -119,11 +133,17 @@ abstract class IncrTransformer[Input <: Identifiable , Output <: Identifiable](c
                 statMap.put(input.identity, Done)
                 output :: outList
             }
-          case Some(Failure(e: Exception)) | None => //I guess I might be able to see a way that this could occur? (Timeout, for example)
+          case Some(Failure(e: Exception)) => //I guess I might be able to see a way that this could occur? (Timeout, for example)
             errMap.put(input.identity, GeneralErrorSummary(e))
+            statMap.put(input.identity, Error)
+            oList
+          case None =>
+            errMap.put(input.identity, GeneralErrorSummary(new Exception("Timed out.")))
+            statMap.put(input.identity, Error)
             oList
           case _ =>
             errMap.put(input.identity, GeneralErrorSummary(new Exception("what?"))) //Should NEVER occur.
+            statMap.put(input.identity, Error)
             oList
         }
     }.reverse
@@ -140,6 +160,15 @@ abstract class IncrTransformer[Input <: Identifiable , Output <: Identifiable](c
               // Already done, log this if verbosity is requested
               None
             }
+            case Error if (errMap.get(inputId) match{
+                case Some(e) =>
+                  def falseLoop(errList: List[ErrorSummary]): Boolean = errList match{
+                    case Nil => true
+                    case errSum :: rest => if (errSum.identity().id == e.identity().id) false else falseLoop(rest)
+                  }
+                  falseLoop(errList)
+                case None => false //Assume that the error status was a fluke.
+              }) => None //There's nothing we can do here.
             case default => {
               // Status is either NotDone or Error. For now, just recompute in both cases
               inputMap.get(inputId) match {
@@ -167,14 +196,15 @@ abstract class IncrTransformer[Input <: Identifiable , Output <: Identifiable](c
       }
     )
     // println(inputs)
-    inputs.flatMap( tryComputeThenStore(_, outputMap) )
+    //inputs.flatMap( tryComputeThenStore(_, outputMap) )
+    multiComputeThenStore(inputs, outputMap)
     outputMap
   }
 }
 
 class FunctionActor[Input <: Identifiable, Output <: Identifiable](func: Input=>List[Output]) extends Actor{
   def receive = {
-    case Some(i: Input) =>
+    case i: Input =>
       try {
         val output = func(i)
         sender() ! output
