b8aa67f767bfffc294cc7989af15474f30ad8f14
Edmund Lam <edmundlam@Edmunds-MacBook-Pro.local>
2017-08-20 23:22:13 -0600

    added Json conversion for Identity classes

--- protopipes/src/main/scala/protopipes/data/Identifiable.scala
+++ protopipes/src/main/scala/protopipes/data/Identifiable.scala
@@ -21,75 +21,6 @@ abstract class Identifiable[A] {
   def setVersion(version: String): Unit = identityOpt = Some(identity().withVersion(version))
 }
 
-object Identity {
-
-  def lift[Src,Dest](identity: Identity[Src]): Identity[Dest] = BasicIdentity[Dest](identity.getId())
-
-  def toAny[Src](identity: Identity[Src]): Identity[Any] = BasicIdentity[Any](identity.getId())
-
-}
-
-abstract class Identity[A] { // extends Identifiable[A] {
-
-  def dropVersion(): BasicIdentity[A]
-
-  def withVersion(v: String): VersionedIdentity[A]
-
-  def getId(): String
-
-}
-
-case class BasicIdentity[A](id: String) extends Identity[A] {
-
-  override def dropVersion(): BasicIdentity[A] = this
-
-  override def withVersion(v: String) = VersionedIdentity[A](id, v)
-
-  override def getId(): String = id
-
-  override def toString: String = id.toString
-
-}
-
-case class VersionedIdentity[A](id: String, version: String) extends Identity[A] {
-
-  def getVersion(): String = version
-
-  override def dropVersion(): BasicIdentity[A] = BasicIdentity[A](id)
-
-  override def withVersion(v: String) = VersionedIdentity[A](id, v)
-
-  override def getId(): String = id
-
-  override def toString: String = s"$id#$version"
-
-}
-
-object PairIdentity {
-
-  def mergeIds[A,B](id1: Identity[A], id2: Identity[B]): String = id1.getId() + ":Pair:" + id2.getId()
-
-  /*
-  def mergeVersions[A,B](id1: Identity[A], id2: Identity[B]): Option[String] = {
-    (id1.identity().version,id2.identity().version) match {
-      case (Some(vL),Some(vR)) => Some(vL + "::" + vR)
-      case (Some(vL),None)     => Some(vL + "::()")
-      case (None,Some(vR))     => Some("()::" + vR)
-      case (None,None)         => None
-    }
-  } */
-
-}
-
-class PairIdentity[L <: Identifiable[L],R <: Identifiable[R]](id1: Identity[L], id2: Identity[R])
-  extends BasicIdentity[Pair[L,R]](PairIdentity.mergeIds(id1,id2))  {
-
-  val leftId = id1
-  val rightId = id2
-
-}
-
-
 abstract class Either[L <: Identifiable[L], R <: Identifiable[R]] extends Identifiable[Either[L,R]]
 
 case class Left[L <: Identifiable[L], R <: Identifiable[R]](left: L) extends Either[L,R] {
@@ -113,6 +44,12 @@ case class Pair[L <: Identifiable[L],R <: Identifiable[R]](left: L, right: R) ex
 case class I[A](a: A) extends Identifiable[I[A]] {
   def i(): A = a
   override def mkIdentity(): Identity[I[A]] = BasicIdentity[I[A]](s"${a.toString()}")
+  override def toString: String = s"${a.toString}"
+}
+
+case class U[A](a: A) extends Identifiable[U[A]] {
+  def u(): A = a
+  override def mkIdentity(): Identity[U[A]] = BasicIdentity[U[A]](java.util.UUID.randomUUID().toString)
   override def toString: String = s"${a.toString}"
 }
 
--- protopipes/src/main/scala/protopipes/data/Identity.scala
+++ protopipes/src/main/scala/protopipes/data/Identity.scala
@@ -4,9 +4,110 @@ package protopipes.data
   * Created by edmundlam on 8/8/17.
   */
 
-abstract class IdentityCoerce[A] {
+object Identity {
 
-  def a[A]: Unit
+  def lift[Src,Dest](identity: Identity[Src]): Identity[Dest] = BasicIdentity[Dest](identity.getId())
+
+  def toAny[Src](identity: Identity[Src]): Identity[Any] = BasicIdentity[Any](identity.getId())
+
+}
+
+abstract class Identity[A] { // extends Identifiable[A] {
+
+  def dropVersion(): BasicIdentity[A]
+
+  def withVersion(v: String): VersionedIdentity[A]
+
+  def getId(): String
+
+}
+
+case class BasicIdentity[A](id: String) extends Identity[A] {
+
+  override def dropVersion(): BasicIdentity[A] = this
+
+  override def withVersion(v: String) = VersionedIdentity[A](id, v)
+
+  override def getId(): String = id
+
+  override def toString: String = s"Id($id)"
+
+}
+
+case class VersionedIdentity[A](id: String, version: String) extends Identity[A] {
+
+  def getVersion(): String = version
+
+  override def dropVersion(): BasicIdentity[A] = BasicIdentity[A](id)
+
+  override def withVersion(v: String) = VersionedIdentity[A](id, v)
+
+  override def getId(): String = id
+
+  override def toString: String = s"Id($id)#$version"
+
+}
+
+import spray.json._
+
+object IdentityToJson extends DefaultJsonProtocol {
+  implicit def basicIdentityToJson[A] = jsonFormat1(BasicIdentity[A])
+  implicit def VersionedIdentityToJson[A] = jsonFormat2(VersionedIdentity[A])
+}
+
+import IdentityToJson._
+
+class Dummy
+
+object tester {
+
+  def main(args: Array[String]): Unit = {
+
+    val id = BasicIdentity[Dummy]("happy")
+
+    val json = id.toJson
+
+    println(json.toString())
+
+    val reId = json.convertTo[BasicIdentity[Dummy]]
+
+    println(reId)
+
+    val vid = id.withVersion("v0.2")
+
+    val vjson = vid.toJson
+
+    println(vjson.toString())
+
+    val reVid = vjson.convertTo[VersionedIdentity[Dummy]]
+
+    println(reVid)
+
+  }
+
+}
+
+object PairIdentity {
+
+  def mergeIds[A,B](id1: Identity[A], id2: Identity[B]): String = id1.getId() + ":Pair:" + id2.getId()
+
+  /*
+  def mergeVersions[A,B](id1: Identity[A], id2: Identity[B]): Option[String] = {
+    (id1.identity().version,id2.identity().version) match {
+      case (Some(vL),Some(vR)) => Some(vL + "::" + vR)
+      case (Some(vL),None)     => Some(vL + "::()")
+      case (None,Some(vR))     => Some("()::" + vR)
+      case (None,None)         => None
+    }
+  } */
+
+}
+
+class PairIdentity[L <: Identifiable[L],R <: Identifiable[R]](id1: Identity[L], id2: Identity[R])
+  extends BasicIdentity[Pair[L,R]](PairIdentity.mergeIds(id1,id2))  {
+
+  val leftId = id1
+  val rightId = id2
 
 }
 
