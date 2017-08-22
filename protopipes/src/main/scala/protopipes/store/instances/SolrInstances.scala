457353b76d9a41a51e201c296566bf2f364a165c
PlatPlat48 <roberts.chance@gmail.com>
2017-08-22 16:06:19 -0600

    SolrStores Mostly Re-implemented.
    I’ve mostly re-implemented every part of the SolrDataMap and the
    SolrDataMultiMap, but I still have to re-implement the extract and size
    methods. I need to finish these, and cut down the SolrSpecific trait a
    little bit!

--- protopipes/src/main/scala/protopipes/store/instances/SolrInstances.scala
+++ protopipes/src/main/scala/protopipes/store/instances/SolrInstances.scala
@@ -1,8 +1,12 @@
 package protopipes.store.instances
 
 import protopipes.data.Identifiable
+import protopipes.data.serializers.JsonSerializer
+import protopipes.exceptions.CallNotAllowException
 import protopipes.store.{DataMap, DataMultiMap, DataStore}
+import spray.json._
 
+import scala.util.Random
 import scala.util.parsing.json.JSON
 import scalaj.http.Http
 
@@ -19,117 +23,87 @@ object SolrInstances{
   }
 }
 
-trait SolrSpecific[Key, Data <: Identifiable[Data]] {
-  var name = ""
-  var url = ""
+trait SolrSpecific[Key, Data <: Identifiable[Data]] extends JsonSerializer[Data]{
+  var nam = ""
+  var url = "http://localhost:8983/solr/gettingstarted/"
   def init(nm: String, URL: String): Unit = {
-    name = nm
+    nam = nm
     url = URL
   }
 
   def keyToString(key: Key): String = {
     key match {
       case i: Identifiable[_] => i.getId().toString
-      case _ => toJSON(key)
+      case _ => key.toString
     }
   }
 
-  def toJson(d: Data): String
+  //def toJson(d: Data): String
 
-  def fromJson(s: String): Data
+  //def fromJson(s: String): Data
 
-  def toJSON(a: Any): String = a match{
-    case l: List[_] =>
-      l.tail.foldLeft(s"[ ${toJSON(l.head)}") {
-        case (str, next) => s"$str, ${toJSON(next)}"
-      } + " ]"
-    case m: Map[_, _] => m.tail.foldLeft(s" [ ${toJSON(m.head._1)}: ${toJSON(m.head._2)}"){
-      case (str, (k, v)) => s"$str, ${toJSON(k)}: ${toJSON(v)}"
-    } + " }"
-    case s: String => "\"" + s.replaceAll("\"", "\\\"") + "\""
-    case _ => a.toString.replaceAll("\"", "\\\"")
-  }
-
-  def getDocument(id: String = name): Option[Map[String, Any]] = {
-    def getDocumentFromList(list: List[Map[String, Any]], id: String = name): Option[Map[String, Any]] = list match{
+  def getDocument(id: String): Option[JsObject] = {
+    def getDocumentFromList(list: List[JsObject], id: String): Option[JsObject] = list match{
       case Nil => None
-      case first :: rest => first.get("id") match{
-        case Some(x) if x.equals(id) => Some(first)
+      case first :: rest => first.fields.get("id") match{
+        case Some(x: JsString) if x.value.equals(id) => Some(first)
         case _ => getDocumentFromList(rest, id)
       }
     }
     val queryURL = url+"select?wt=json&q=id=\"" + id + "\""
     val json = Http(queryURL).asString.body
-    JSON.parseFull(json) match{
-      case Some(parsed: Map[String@unchecked, Any@unchecked]) => parsed.get("response") match{
-        case Some(response: Map[String@unchecked, Any@unchecked]) => response.get("docs") match{
-          case Some(docs: List[Map[String, Any]@unchecked]) => getDocumentFromList(docs)
-        }
-        case None => None
+    json.parseJson.asJsObject().fields.get("response") match{
+      case Some(response: JsValue) => response.asJsObject.fields.get("docs")  match{
+        case Some(docs: JsArray) => getDocumentFromList(docs.elements.toList.map(_.asJsObject), id)
       }
-      case None => None
     }
   }
 
-  def getDocuments(id: String = name, field: String = "_id_", rows: String = "1000000000"): List[Map[String, Any]] = {
+  def getDocuments(id: String, field: String = "_id_", rows: String = "1000000000"): List[JsObject] = {
     val queryURL = url+"select?wt=json&rows=" + rows + "&q="+field+"=\""+ id + "\""
     val json = Http(queryURL).asString.body
-    JSON.parseFull(json) match{
-      case Some(parsed: Map[String@unchecked, Any@unchecked]) => parsed.get("response") match{
-        case Some(response: Map[String@unchecked, Any@unchecked]) => response.get("docs") match{
-          case Some(docs: List[Map[String, Any]@unchecked]) => docs.foldRight(List[Map[String, Any]]()){
-            case (m: Map[String, Any], list) => m.get(field) match{
-              case Some(str: String) if str.equals(id) => m :: list
+    json.parseJson.asJsObject.fields.get("response") match{
+      case Some(response: JsValue) => response.asJsObject.fields.get("docs") match{
+        case Some(docs: JsArray) => docs.elements.foldRight(List[JsObject]()){
+          case (v: JsValue, list) =>
+            val obj = v.asJsObject
+            obj.fields.get(field) match{
+              case Some(str: JsString) if str.value.equals(id) => obj :: list
               case _ => list
             }
-          }
         }
-        case None => List()
+        case _ => List()
       }
-      case None => List()
+      case _ => List()
     }
   }
 
-  def addDocument(doc: Map[String, Any], key: Key): Unit = {
-    val jsonMap = Map("add" -> Map("doc" -> doc), "commit" -> Map[String, Any]())
-    val json = toJSON(jsonMap)
+  def addDocument(doc: JsObject, key: Key): Unit = {
+    val json = JsObject(Map("add" -> JsObject(Map("doc" -> doc)), "commit" -> JsObject())).toString()
     val result = Http(url+"update").postData(json.getBytes).header("Content-Type", "application/json").asString.body
-    JSON.parseFull(result) match{
-      case Some(parsed: Map[String @ unchecked, Any @ unchecked]) =>
-        parsed.get("error") match{
-          case Some(x) => throw new Exception(s"Could not update the DataMap $name on key(s) $key.")
-          case _ => ()
-        }
+    result.parseJson.asJsObject.fields.get("error") match{
+      case Some(x) => throw new Exception(s"Could not update the DataMap $nam on key(s) $key.")
       case _ => ()
     }
   }
 
   def deleteDocuments(ids: List[String]): Unit = {
-    val jsonMap = Map("delete" -> ids)
-    val json = toJSON(jsonMap)
+    val json = JsObject(Map("delete" -> JsArray(ids.map(JsString(_)).toVector))).toString
     val result = Http(url+"update").postData(json.getBytes).header("Content-Type", "application/json").asString.body
-    JSON.parseFull(result) match{
-      case Some(parsed: Map[String @ unchecked, Any @ unchecked]) =>
-        parsed.get("error") match{
-          case Some(x) => throw new Exception(s"Could not update the DataMap $name on ID(s) $ids.")
-          case _ => ()
-        }
+    result.parseJson.asJsObject.fields.get("error") match{
+      case Some(x) => throw new Exception(s"Could not update the DataMap $nam on ID(s) $ids.")
       case _ => ()
     }
   }
 
-  def getAllDocuments: List[Map[String, Any]] = {
+  def getAllDocuments: List[JsObject] = {
     val queryURL = url+"select?&rows=100000000&q=*:*"
     val json = Http(queryURL).asString.body
-    JSON.parseFull(json) match {
-      case Some(parsed: Map[String@unchecked, Any@unchecked]) => parsed.get("response") match {
-        case Some(response: Map[String@unchecked, Any@unchecked]) => response.get("docs") match {
-          case Some(docs: List[Map[String, Any]@unchecked]) => docs
-          case _ => List()
-        }
+    json.parseJson.asJsObject.fields.get("response") match {
+      case Some(response: JsValue) => response.asJsObject.fields.get("docs") match{
+        case Some(docs: JsArray) => docs.elements.toList.map(_.asJsObject)
         case _ => List()
       }
-      case _ => List()
     }
   }
 }
@@ -142,44 +116,31 @@ abstract class SolrDataMap[Key, Data <: Identifiable[Data]](coreName: String, so
 
   override def put_(key: Key, data: Data): Unit = {
     val kToString = keyToString(key)
-    val dMap: String = data match {
-      case i: Identifiable[_] => toJSON(Map("id" -> i.getId()))
-      case _ => toJSON(data)
-    }
-    val document = getDocument(kToString) match {
-      case Some(m: Map[String@unchecked, Any@unchecked]) =>
-        val newM = m - "_version_"
-        newM + ("dataID" -> dMap, "data" -> toJson(data))
-      case None =>
-        Map("id" -> kToString, "dataID" -> dMap, "data" -> toJson(data))
-    }
-    addDocument(document, key)
+    val document = serializeToJson(data)
+    val doc = JsObject(document.fields + ("id" -> JsString(data.getId())))
+    addDocument(doc, key)
   }
 
   override def put_(data: Seq[Data]): Unit = {}
 
   override def get(key: Key): Option[Data] = {
-    getDocument() match {
-      case Some(m: Map[String@unchecked, Any@unchecked]) => m.get("data") match {
-        case Some(data: String) => Some(fromJson(data))
-        case Some((data: String) :: _) => Some(fromJson(data))
-        case _ => None
-      }
-      case None => None
+    getDocument(keyToString(key)) match{
+      case Some(jsObject: JsObject) =>
+        Some(deserialize(JsObject(jsObject.fields -- List("id", "_version_")).toString))
+      case _ => None
     }
   }
 
-  override def contains(key: Key): Boolean = getDocument() match {
-    case Some(m: Map[String@unchecked, Any@unchecked]) => true
+  override def contains(key: Key): Boolean = getDocument(keyToString(key)) match {
+    case Some(_) => true
     case _ => false
   }
 
   override def all(): Seq[Data] = {
     getAllDocuments.foldRight(List[Data]()) {
-      case (map: Map[String@unchecked, Any@unchecked], data) =>
-        map.get("data") match {
-          case Some(jsonable: String) => fromJson(jsonable) :: data
-        }
+      case (jsObj, data) =>
+        val jsonable = JsObject(jsObj.fields -- List("id", "_version_")).toString
+        deserialize(jsonable) :: data
     }
   }
 
@@ -202,13 +163,10 @@ abstract class SolrDataMap[Key, Data <: Identifiable[Data]](coreName: String, so
   override def size(): Int = {
     val queryURL = url+"select?wt=json&q=*:*"
     val json = Http(queryURL).asString.body
-    JSON.parseFull(json) match{
-      case Some(parsed: Map[String@unchecked, Any@unchecked]) => parsed.get("response") match{
-        case Some(response: Map[String@unchecked, Any@unchecked]) => parsed.get("numFound") match {
-          case Some(num: Int) => num
-          case Some(str: String) => str.toInt
-          case _ => 0
-        }
+    json.parseJson.asJsObject.fields.get("response") match {
+      case Some(response: JsValue) => response.asJsObject.fields.get("numFound") match{
+        case Some(num: JsNumber) => num.value.intValue()
+        case Some(str: JsString) => str.value.toInt
         case _ => 0
       }
       case _ => 0
@@ -229,11 +187,9 @@ abstract class SolrMultiMap[Key, Data <: Identifiable[Data]](coreName: String, s
   override def put_(key: Key, data: Set[Data]): Unit = {
     val kToString = keyToString(key)
     data.foreach{dta =>
-      val dMap: String = dta match {
-        case i: Identifiable[_] => toJSON(Map("id" -> i.getId()))
-        case _ => toJson(dta)
-      }
-      val doc = Map[String, Any]("id" -> s"${kToString}___$dMap", "data" -> toJson(dta), "dataID" -> dMap, "_id_" -> kToString)
+      val document = serializeToJson(dta)
+      val doc = JsObject(document.fields +
+        ("_id_" -> JsString(dta.getId()), "id" -> JsString(s"${kToString}___${dta.getId()}_${Random.nextInt(99999)}")))
       addDocument(doc, key)
     }
   }
@@ -244,8 +200,8 @@ abstract class SolrMultiMap[Key, Data <: Identifiable[Data]](coreName: String, s
 
   override def remove(key: Key): Unit = {
     deleteDocuments(getDocuments(keyToString(key)).foldRight(List[String]()) {
-      case (map, list) => map.get("id") match {
-        case Some(s: String) => s :: list
+      case (jsObj, list) => jsObj.fields.get("id") match{
+        case Some(s: JsString) => s.value :: list
         case _ => list
       }
     })
@@ -253,53 +209,53 @@ abstract class SolrMultiMap[Key, Data <: Identifiable[Data]](coreName: String, s
 
   override def remove(key: Key, data: Set[Data]): Unit = {
     val serializedMap = data.foldRight(Map[String, Unit]()) {
-      case (dta, map) => map + (toJson(dta) -> ())
+      case (dta, map) => map + (serialize(dta) -> ())
     }
-    deleteDocuments(getDocuments(keyToString(key)).foldRight(List[String]()) {
-      case (map, toRemove) => (map.get("id"), map.get("data")) match {
-        case (Some(id: String), Some(dt: String)) => serializedMap.get(dt) match {
-          case Some(_) => id :: toRemove
+    deleteDocuments(getDocuments(keyToString(key)).foldRight(List[String]()){
+      case (jsObj, toRemove) =>
+        serializedMap.get(JsObject(jsObj.fields -- List("id", "_version_", "_id_")).toString()) match {
+          case Some(_) => jsObj.fields.get("id") match{
+            case Some(id: JsString) => id.value :: toRemove
+            case _ => toRemove
+          }
           case _ => toRemove
         }
-      }
     })
   }
 
   override def remove(data: Set[Data]): Unit = {
     val serializedMap = data.foldRight(Map[String, Unit]()) {
-      case (dta, map) => map + (toJson(dta) -> ())
+      case (dta, map) => map + (serialize(dta) -> ())
     }
-    val toDelete = getAllDocuments.foldRight(List[String]()){
-      case (doc, toRemove) => (doc.get("id"), doc.get("data")) match{
-        case (Some(id: String), Some(dta: String)) => serializedMap.get(dta) match{
-          case Some(_) => id :: toRemove
+    deleteDocuments(getAllDocuments.foldRight(List[String]()){
+      case (jsObj, toRemove) =>
+        serializedMap.get(JsObject(jsObj.fields -- List("id", "_version_", "_id_")).toString()) match {
+          case Some(_) => jsObj.fields.get("id") match{
+            case Some(id: JsString) => id.value :: toRemove
+            case _ => toRemove
+          }
           case _ => toRemove
         }
-      }
-    }
-    deleteDocuments(toDelete)
+    })
   }
 
-  override def iterator(): Iterator[Set[Data]] = ???
+  override def iterator(): Iterator[Set[Data]] = throw new CallNotAllowException(???, None)
 
   override def iterator(key: Key): Iterator[Data] = ???
 
   override def get(key: Key): Set[Data] = {
     getDocuments(keyToString(key)).foldRight(Set[Data]()) {
-      case (m, set) => m.get("data") match {
-        case Some(str: String) => set + fromJson(str)
-        case None => set
-      }
+      case (jsObj, set) => set + deserialize(jsObj.toString)
     }
   }
 
   override def all(): Seq[Set[Data]] = {
-    getAllDocuments.foldRight(Map[String, Set[Data]]()) {
-      case (map: Map[String@unchecked, Any@unchecked], data) =>
-        (map.get("data"), map.get("_id_")) match {
-          case (Some(jsonable: String), Some(iden: String)) => data.get(iden) match {
-            case Some(x) => data + (iden -> (x + fromJson(jsonable)))
-            case None => data + (iden -> Set(fromJson(jsonable)))
+    getAllDocuments.foldRight(Map[JsString, Set[Data]]()) {
+      case (jsObj, data) =>
+        jsObj.fields.get("_id_") match{
+          case Some(id: JsString) => data.get(id) match {
+            case Some(set) => data + (id -> (set + deserialize(jsObj.toString)))
+            case None => data + (id -> Set(deserialize(jsObj.toString)))
           }
           case _ => data
         }
@@ -308,22 +264,25 @@ abstract class SolrMultiMap[Key, Data <: Identifiable[Data]](coreName: String, s
 
   override def contains(key: Key, data: Set[Data]): Boolean = data subsetOf get(key)
 
-  override def extract(): Seq[Set[Data]] = {
+  override def extract(): Seq[Set[Data]] = { ???
+    /*
     val (mapOfSets, stuffToRemove) = getAllDocuments.foldRight((Map[String, Set[Data]](), List[String]())) {
       case (map: Map[String@unchecked, Any@unchecked], (data, toRemove)) =>
         (map.get("data"), map.get("_id_"), map.get("id")) match {
           case (Some(jsonable: String), Some(iden: String), Some(id: String)) => data.get(iden) match {
-            case Some(x) => (data + (iden -> (x + fromJson(jsonable))), id :: toRemove)
-            case None => (data + (iden -> Set(fromJson(jsonable))), id :: toRemove)
+            case Some(x) => (data + (iden -> (x + deserialize(jsonable))), id :: toRemove)
+            case None => (data + (iden -> Set(deserialize(jsonable))), id :: toRemove)
           }
           case _ => (data, toRemove)
         }
     }
     deleteDocuments(stuffToRemove)
     mapOfSets.toSeq.unzip._2
+    */
   }
 
-  override def size(): Int = {
+  override def size(): Int = { ???
+    /*
     getAllDocuments.foldRight(Map[String, Unit]()) {
       case (map, keysFound) =>
         map.get("_id_") match {
@@ -333,6 +292,7 @@ abstract class SolrMultiMap[Key, Data <: Identifiable[Data]](coreName: String, s
           }
         }
     }.size
+    */
   }
 }
 
